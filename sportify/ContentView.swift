62aff46a252b5b580d1d8c1315ea61a681f85da2
Vishaal <vschittur@gmail.com>
2021-03-15 12:35:59 -0400

    Initial Commit

---
+++ sportify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sportify
+//
+//  Created by Vishaal Chittur on 2021-03-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sportify/sportifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportifyApp.swift
+//  sportify
+//
+//  Created by Vishaal Chittur on 2021-03-15.
+//
+
+import SwiftUI
+
+@main
+struct sportifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
