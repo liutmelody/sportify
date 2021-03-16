016887c4f8c617b836676f064b43063357edb9cb
Vishaal <vschittur@gmail.com>
2021-03-16 15:39:04 -0400

    first commit

--- sportify/ContentView.swift
+++ sportify/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Text("Hello, world!")
+//            .padding()
+
+        VStack(spacing: 50) {
+            Image(uiImage: #imageLiteral(resourceName: "Group 45"))
+            Image(uiImage: #imageLiteral(resourceName: "Sportify"))
+            Image(uiImage: #imageLiteral(resourceName: "Play sports with your neighbors"))
+            ZStack {
+                Image(uiImage: #imageLiteral(resourceName: "Rectangle 13"))
+                Image(uiImage: #imageLiteral(resourceName: "Group 55"))
+            }
+        }
     }
 }
 
